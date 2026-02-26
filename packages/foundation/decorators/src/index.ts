c98b1aad44b1253fb63207f78112488cf05accb6
Mateus Martins <mateusam.ofc@outlook.com>
2026-02-26 20:00:47 -0300

    refactor(decorators): streamline exports in index.ts for improved module clarity
    - Updated exports in `index.ts` to explicitly list components, functions, properties, and events for better readability and maintainability.
    - This change enhances the clarity of the module's public API by specifying the exported items directly.

--- packages/foundation/decorators/src/index.ts
+++ packages/foundation/decorators/src/index.ts
@@ -1,4 +1,4 @@
-export * from "./component";
-export * from "./functions";
-export * from "./properties";
-export * from "./events";
+export { Component, type ComponentOptions, Momoize, Lazy, Virtual } from "./component";
+export { Memo, Bind, Log, Once, Retry, Debounce, Throttle, When, Watch, type WatchVal, type WatchVals } from "./functions";
+export { Prop, State, Persisted, Slot, initSlots, History } from "./properties";
+export { type Command, createCommand, Emit, OnCommand } from "./events";
